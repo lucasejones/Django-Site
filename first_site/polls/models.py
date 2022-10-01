c2b05a76e89ea6088daaf6d75751e5e50f020edc
Lucas Jones <lucasejones+1@gmail.com>
2022-10-01 10:02:08 -0800

    Performing Database Setup
    I've adjusted the time zone to local time in settings.py.
    
    I then ran `$ python manage.py migrate`. this generate the required tables, as dictated by the installed apps, also found in settings.py.
    
    Finally, I've also begun defining some models which will add relevant shape and content to the database.

--- first_site/polls/models.py
+++ first_site/polls/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
 # Create your models here.
+
+'''
+Comprehension Notes:
+Each model has a number of variables.
+	- Each variable represents a db field. 
+Each field is represented by an instance of a Field class.
+The variable names, (the content of which holdseach field instance) will be the column names in the db.
+You can create an optional name as the first argument, as shown below in Question.pub_date: "date published".
+''' 
+
+class Question(models.Model):
+	question_text = models.CharField(max_length=200)
+	pub_date = models.DateTimeField('date published')
+
+
+class Choice(models.Model):
+	question = models.ForeignKey(Question, on_delete=models.CASCADE)
+	choice_text = models.CharField(max_length=200)
+	votes = models.IntegerField(default=0)
+
+
